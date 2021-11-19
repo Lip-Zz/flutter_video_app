ec8c3b61b09084cdd38ce223d9f25d1d2985b033
zz <zhaolaoda2011@gmail.com>
2021-11-19 15:30:24 +0800

    个人中心

--- video/lib/page/my_page.dart
+++ video/lib/page/my_page.dart
@@ -1,4 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:video/httpUtils/core/hi_error.dart';
+import 'package:video/httpUtils/dao/home_dao.dart';
+import 'package:video/model/homeModel.dart';
+import 'package:video/util/format_util.dart';
+import 'package:video/util/hi_blur.dart';
+import 'package:video/util/toast.dart';
+import 'package:video/util/view_util.dart';
+import 'package:video/wiget/hi_banner.dart';
+import 'package:video/wiget/hi_flexble_header.dart';
 
 class MyPage extends StatefulWidget {
   MyPage({Key? key}) : super(key: key);
@@ -7,22 +17,141 @@ class MyPage extends StatefulWidget {
   _MyPageState createState() => _MyPageState();
 }
 
-class _MyPageState extends State<MyPage> {
+class _MyPageState extends State<MyPage> with AutomaticKeepAliveClientMixin {
+  HomeModel? mo;
+  late ScrollController _controller;
+
   @override
   void initState() {
     super.initState();
+    _controller = ScrollController();
     _loadData();
   }
 
   @override
   Widget build(BuildContext context) {
+    super.build(context);
     return Scaffold(
-      appBar: AppBar(),
-      body: Container(
-        child: Text("我的"),
+      body: NestedScrollView(
+        controller: _controller,
+        headerSliverBuilder: (context, b) {
+          return [_appbar()];
+        },
+        body: ListView(
+          padding: EdgeInsets.only(top: 10),
+          children: [...content()],
+        ),
       ),
     );
   }
 
-  void _loadData() {}
+  void _loadData() async {
+    try {
+      HomeModel result = await HomeDao.get("推荐");
+      print(result);
+
+      this.setState(() {
+        mo = result;
+      });
+    } on NeedAuth catch (e) {
+      showWarnToast(e.message);
+    } on HiNETError catch (e) {
+      showWarnToast(e.message);
+    }
+  }
+
+  @override
+  bool get wantKeepAlive => true;
+
+  _info() {
+    return HiFlexbleHeader(
+      name: mo?.name,
+      face: mo?.face,
+      controller: _controller,
+    );
+  }
+
+  _bg() {
+    return Stack(
+      children: [
+        Positioned.fill(child: cacheNetworkImage(mo?.face ?? '')),
+        Positioned.fill(
+          child: HiBlur(
+            sigma: 20,
+          ),
+        ),
+        Positioned(
+          child: _profile(),
+          left: 0,
+          right: 0,
+          bottom: 0,
+        )
+      ],
+    );
+  }
+
+  _appbar() {
+    return SliverAppBar(
+      expandedHeight: 160,
+      pinned: true,
+      flexibleSpace: FlexibleSpaceBar(
+        collapseMode: CollapseMode.parallax,
+        titlePadding: EdgeInsets.only(left: 0),
+        title: _info(),
+        background: _bg(),
+      ),
+    );
+  }
+
+  content() {
+    //不写保护的话，banner会疯狂滚动，然后恢复正常
+    if (mo == null) {
+      return [];
+    }
+    return [_banner()];
+  }
+
+  _profile() {
+    if (mo == null) {
+      return Container();
+    }
+
+    return Container(
+      padding: EdgeInsets.only(top: 5, bottom: 5),
+      decoration: BoxDecoration(color: Colors.white54),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          _infoText('收藏', mo?.favorite ?? 0),
+          _infoText('点赞', mo?.like ?? 0),
+          _infoText('浏览', mo?.browsing ?? 0),
+          _infoText('金币', mo?.coin ?? 0),
+          _infoText('粉丝数', mo?.fans ?? 0),
+        ],
+      ),
+    );
+  }
+
+  _banner() {
+    return HiBanner(
+      bannerList: mo?.bannerList ?? [],
+      bannerHeight: 120,
+      padding: EdgeInsets.only(left: 10, right: 10),
+    );
+  }
+
+  _infoText(String s, int i) {
+    return Column(
+      children: [
+        Text(
+          s,
+          style: TextStyle(fontSize: 15, color: Colors.black87),
+        ),
+        Text(
+          countFormat(i),
+          style: TextStyle(fontSize: 12, color: Colors.grey[600]),
+        )
+      ],
+    );
+  }
 }
---
+++ video/lib/wiget/hi_flexble_header.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+import 'package:video/util/view_util.dart';
+
+class HiFlexbleHeader extends StatefulWidget {
+  final String? name;
+  final String? face;
+  final ScrollController? controller;
+  HiFlexbleHeader({Key? key, this.name, this.face, this.controller})
+      : super(key: key);
+
+  @override
+  _HiFlexbleHeaderState createState() => _HiFlexbleHeaderState();
+}
+
+class _HiFlexbleHeaderState extends State<HiFlexbleHeader> {
+  static const double MAX_BOTTOM_PADDING = 40;
+  static const double MIN_BOTTOM_PADDING = 10;
+  static const double MAX_OFFSET = 80; //滚动距离
+
+  //动态变化的底部间距
+  double _dyBottomPadding = MAX_BOTTOM_PADDING;
+
+  @override
+  void initState() {
+    super.initState();
+    widget.controller?.addListener(() {
+      var offset = widget.controller?.offset ?? 0;
+      //计算滚动距离的占比
+      var dyOffset = (MAX_OFFSET - offset) / MAX_OFFSET;
+      //计算出具体的padding值
+      var dy = dyOffset * (MAX_BOTTOM_PADDING - MIN_BOTTOM_PADDING);
+      if (dy > (MAX_BOTTOM_PADDING - MIN_BOTTOM_PADDING)) {
+        dy = MAX_BOTTOM_PADDING - MIN_BOTTOM_PADDING;
+      } else if (dy < 0) {
+        dy = 0;
+      }
+
+      setState(() {
+        _dyBottomPadding = MIN_BOTTOM_PADDING + dy;
+      });
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      alignment: Alignment.bottomLeft,
+      padding: EdgeInsets.only(bottom: _dyBottomPadding, left: 10),
+      child: Row(
+        children: [
+          ClipRRect(
+            borderRadius: BorderRadius.circular(23),
+            child: cacheNetworkImage(widget.face ?? '', width: 46, height: 46),
+          ),
+          hiSpace(width: 8),
+          Text(
+            widget.name ?? '',
+            style: TextStyle(fontSize: 11, color: Colors.black87),
+          )
+        ],
+      ),
+    );
+  }
+}
